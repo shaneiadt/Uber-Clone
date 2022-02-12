79f51415d8ee7eb2600540221180351632c32d5d
Shane O'Moore <shaneiadt@gmail.com>
2022-02-12 12:23:50 +0000

    basic option element style

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,6 @@
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames';
 
 const data = [
     {
@@ -23,12 +24,13 @@ const NavOptions = () => {
             horizontal
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <TouchableOpacity>
+                <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                     <View>
                         <Image
                             style={{ width: 120, height: 120, resizeMode: 'contain' }}
                             source={{ uri: item.image }}
                         />
+                        <Text>{item.title}</Text>
                     </View>
                 </TouchableOpacity>
             )}
