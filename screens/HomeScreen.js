81d515400f6c2d667a2cec266b65a945c37312f9
Shane O'Moore <shaneiadt@gmail.com>
2022-02-12 15:31:00 +0000

    auto complete component and api key

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { Text, SafeAreaView, Image, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const HomeScreen = () => {
     return (
