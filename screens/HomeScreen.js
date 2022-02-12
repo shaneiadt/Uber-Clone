899fe75cd098947a0f031f2134e097076041098b
Shane O'Moore <shaneiadt@gmail.com>
2022-02-12 15:25:07 +0000

    full height view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView style={tw`bg-white`}>
+        <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
                 <Image
                     style={{ width: 100, height: 100, resizeMode: 'contain' }}
