305295e7f0137c65947355191080ca4114990fcf
Shane O'Moore <shaneiadt@gmail.com>
2022-02-14 12:07:04 +0000

    stack navigator

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,37 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
-import Mapper from '../components/Mapper'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import LocationMap from '../components/LocationMap'
+import NavigateCard from '../components/NavigateCard'
+import RideOptions from '../components/RideOptions'
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
-        <Mapper />
+        <LocationMap />
       </View>
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='NavigateCard'
+            component={NavigateCard}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name='RideOptions'
+            component={RideOptions}
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
