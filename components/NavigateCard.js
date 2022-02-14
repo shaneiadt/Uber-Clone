8c7e3be29a5fd13c10d39c974b0208ead9d54c71
Shane O'Moore <shaneiadt@gmail.com>
2022-02-14 12:25:36 +0000

    display autocomplete "where to?"

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,18 +1,66 @@
 import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { useDispatch } from 'react-redux'
 
-export class NavigateCard extends Component {
-    render() {
-        return (
-            <SafeAreaView
-                style={tw`bg-white flex-1`}
-            >
-                <Text style={tw`text-center py-5 text-xl`}>Good Morning, Shane</Text>
-            </SafeAreaView>
-        )
-    }
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { setDestination } from '../features/navSlice/navSlice'
+import { useNavigation } from '@react-navigation/native'
+
+const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView
+            style={tw`bg-white flex-1`}
+        >
+            <Text style={tw`text-center py-5 text-xl`}>Good Morning, Shane</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where To?'
+                        styles={{
+                            container: {
+                                backgroundColor: 'white',
+                                paddingTop: 20,
+                                flex: 0
+                            },
+                            textInput: {
+                                backgroundColor: '#DDDDDF',
+                                borderRadius: 0,
+                                fontSize: 18
+                            },
+                            textInputContainer: {
+                                paddingHorizontal: 20,
+                                paddingBottom: 0
+                            }
+                        }}
+                        onPress={(data, details = null) => {
+                            dispatch(setDestination({
+                                location: details.geometry.location,
+                                description: data.description
+                            }))
+
+                            navigation.navigate('RideOptions');
+                        }}
+                        returnKeyType='search'
+                        fetchDetails={true}
+                        minLength={2}
+                        enablePoweredByContainer={false}
+                        debounce={500}
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: 'en'
+                        }}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 export default NavigateCard
