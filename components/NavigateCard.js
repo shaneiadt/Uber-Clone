fead3d159006cb4eae4df6ccb0dfff2b1a939474
Shane O'Moore <shaneiadt@gmail.com>
2022-02-17 09:59:01 +0000

    styling bottom ride options buttons

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
@@ -9,6 +9,7 @@ import { GOOGLE_MAPS_APIKEY } from '@env';
 import { setDestination } from '../features/navSlice/navSlice'
 import { useNavigation } from '@react-navigation/native'
 import NavFavorites from './NavFavorites'
+import { Icon } from 'react-native-elements/dist/icons/Icon'
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -60,6 +61,30 @@ const NavigateCard = () => {
                     />
                 </View>
                 <NavFavorites />
+            </View>
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity
+                    style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon
+                        name='car'
+                        type='font-awesome'
+                        color='white'
+                        size={16}
+                    />
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon
+                        name='fast-food-outline'
+                        type='ionicon'
+                        color='black'
+                        size={16}
+                    />
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
