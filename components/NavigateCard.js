f28e82b634b82c3532f1918e668661297171781a
Shane O'Moore <shaneiadt@gmail.com>
2022-02-18 14:47:17 +0000

    navigate to ride options

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -64,6 +64,7 @@ const NavigateCard = () => {
             </View>
             <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
                 <TouchableOpacity
+                    onPress={() => navigation.navigate('RideOptions')}
                     style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
                 >
                     <Icon
