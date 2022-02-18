9b371576ddda634ed44214bbc1337181ee3743ab
Shane O'Moore <shaneiadt@gmail.com>
2022-02-18 14:56:21 +0000

    select a ride text

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,11 +1,12 @@
 import { Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames'
 
 const RideOptions = () => {
   return (
-    <SafeAreaView>
-      <Text>RideOptions</Text>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <Text style={tw`text-center mb-2 font-semibold`}>Select a Ride</Text>
     </SafeAreaView>
   )
 }
