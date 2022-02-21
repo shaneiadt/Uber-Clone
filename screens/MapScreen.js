73729cdb7756791494d65c060402848cf9dd8789
Shane O'Moore <shaneiadt@gmail.com>
2022-02-21 14:24:52 +0000

    touchable back button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -6,12 +6,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LocationMap from '../components/LocationMap'
 import NavigateCard from '../components/NavigateCard'
 import RideOptions from '../components/RideOptions'
+import { Icon } from 'react-native-elements/dist/icons/Icon'
+import { useNavigation } from '@react-navigation/native'
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
-  
+  const navigation = useNavigation();
+
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('HomeScreen')}
+        style={tw`absolute top-6 left-6 bg-gray-100 z-50 p-3 rounded-full shadow-lg`}>
+        <Icon
+          name="menu"
+        />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <LocationMap />
       </View>
